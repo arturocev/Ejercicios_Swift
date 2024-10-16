f85ee1afeb90262c78a31b1fbe3c0a832194045d
Arturo <arturo_garcia_m-tsapp1oa2324@cev.com>
2024-10-16 10:22:03 +0100

    Ejercicio 6 resuelto y mitad del ejercicio 7

--- Ejercicios_Swift/main.swift
+++ Ejercicios_Swift/main.swift
@@ -44,6 +44,7 @@ print(media)
  
 
 // EJERCICIO 2
+/*
 var continuarJugando = true
 var bala = 0
 var numeroJugador = 1
@@ -93,11 +94,11 @@ repeat {
     }
 } while continuarJugando == true
  
- 
+ */
 
 
 //EJERCICIO 4
-
+/*
 print("Escribe una frase: ")
 var frase : String = readLine()!
 
@@ -120,7 +121,8 @@ for i in 1...primeraPalabra.count{
 
 var convertirFrase = primeraPalabra.joined(separator: " ")
 print(convertirFrase)
-
+*/
+/*
 //EJERCICIO 5
 
 func traducirNumeros(numeroRomano: String) -> Int
@@ -174,6 +176,115 @@ var numeroRomano = readLine()!
 var numeroTraducido = traducirNumeros(numeroRomano: numeroRomano.uppercased())
 
 print("El numero traducido a decimal es: \(numeroTraducido)")
-
+*/
 // EJERCICIO 6
 
+/*
+func traducirNumero2(numeroRomano: String) -> Int
+{
+    let valoresRomanos : [Character: Int] =
+    [
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    ]
+    var resultado = 0
+    let valorPrevio = 1
+    
+    for letra in numeroRomano
+    {
+        if let valor = valoresRomanos[letra] {
+            if valorPrevio < valor
+            {
+                resultado += valor
+            }
+            else
+            {
+                resultado -= valor
+            }
+        }
+    }
+    return resultado
+}
+
+print("----- NÚMEROS ROMANO A DECIMAL -----\n",
+    "Las traducciones son:\n",
+        "- I: 1\n",
+        "- V: 5\n",
+        "- X: 10\n",
+        "- L: 50\n",
+        "- C: 100\n",
+        "- D: 500\n",
+        "- M: 1000\n",
+        "Porfavor, introduce un número romano:")
+var numeroRomano = readLine()!
+var numeroTraducido = traducirNumero2(numeroRomano: numeroRomano.uppercased())
+print("El numero traducido es: \(numeroTraducido)")
+*/
+
+// EJERCICIO 7
+/*
+var numeros = [Int]()
+while numeros.count != 5
+{
+    print("Porfavor, escribe un número: ")
+    let numero = readLine()!
+    if numero.isEmpty
+    {
+        print("ERROR: no has introducido nada")
+    }
+    else if numero.first?.isNumber == false
+    {
+        print("ERROR: no has introducido un número")
+    }
+    else if numero.count > 1
+    {
+        print("ERROR: debes introducir solo un valor")
+    } else
+    {
+        let numero2: Int = Int(numero)!
+        numeros.append(numero2)
+    }
+}
+let suma = numeros.reduce(0, {total, numero in
+    total + numero
+})
+
+let media = suma/5
+print("La media es", media)
+*/
+
+print("Escribe una frase: ")
+var frase : String = readLine()!
+
+var primeraPalabra = frase.split(separator:" ")
+
+
+primeraPalabra.map(<#T##transform: (Substring) throws -> T##(Substring) throws -> T#>)
+
+
+
+
+/*
+for i in 1...primeraPalabra.count{
+    if primeraPalabra[i-1].first == "j"
+    {
+        primeraPalabra[i-1] = "j****"
+    }
+    else if primeraPalabra[i-1].first == "p"
+    {
+        primeraPalabra[i-1] = "p****"
+    }
+    else if primeraPalabra[i-1].first == "m"
+    {
+        primeraPalabra[i-1] = "m****"
+    }
+}
+
+var convertirFrase = primeraPalabra.joined(separator: " ")
+print(convertirFrase)
+ */
